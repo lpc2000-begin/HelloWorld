7e750f1bd45a3546b69f2d1ef8f8c69c96411120
win10lemonc <2577262578@qq.com>
2024-09-11 17:19:35 +0800

    修改sort函数

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include<iostrem>
 using namespace std;
+
+void sort(int arr[],int size)
+{
+    int temp=0;
+    for(int i=0;i<size-1;i++)
+    {
+        for(int j=0;j<size-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
+
 int main()
 {
-    cout<<"hello world";
+    int arr[]={12,2,45,68,21};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    sort(arr,size);
+    return 0;
 }
