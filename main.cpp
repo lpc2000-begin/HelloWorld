d700e4fb82a662adc496ed7938fc3467c76ab32c
win10lemonc <2577262578@qq.com>
2024-09-13 18:12:14 +0800

    添加快速排序实现

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include<iostrem>
 using namespace std;
 
+void sortfast(int arr[],int i,int j)
+{
+    int val=arr[i];
+    int l=i;
+    int r=j;
+    while(l<r)
+    {
+        while(l<r&&arr[r]>=val)
+        {
+            r--;
+        }
+        if(l<r)
+        {
+            arr[l++]=arr[r];
+        }
+        while(l<r&&arr[l]<=val)
+        {
+            l++;
+        }
+        if(l<r)
+        {
+            arr[r--]=arr[l];
+        }
+    }
+    arr[l]=val;
+    sort(arr,i,l-1);
+    sort(arr,l+1,j)
+}
+
 void sort(int arr[],int size)
 {
     int temp=0;
@@ -22,6 +51,12 @@ int main()
 {
     int arr[]={12,2,45,68,21};
     int size=sizeof(arr)/sizeof(arr[0]);
-    sort(arr,size);
+    sortbubble(arr,size);
+    sortfast(arr,0,size);
+    for(int i=0;i<size;i++)
+    {
+        std::cout<<arr[i]<<" ";
+    }
+    std::cout<<endl;
     return 0;
 }
